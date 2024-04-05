a29108ae8072c80cf9878cb2a28451897677bcb1
lidiia <lidiia@LIDA>
2024-04-05 10:56:03 +0300

    Добавьте файлы проекта.

---
+++ laba3EF/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Windows;
+namespace laba3EF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private dartbaseEntities content = new dartbaseEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyDataGrid.ItemsSource = content.GetFullInfo.ToList();
+        }
+    }
+}
